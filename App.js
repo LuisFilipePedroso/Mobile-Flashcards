2477c6d558f6fce53d9a1339e9221c01fd501854
Luis Filipe <luis_filipe42@outlook.com>
2019-03-26 07:48:48 -0300

    Created the animation to flip the card

--- App.js
+++ App.js
@@ -1,13 +1,89 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  Animated,
+  TouchableOpacity,
+  SafeAreaView,
+  Easing
+} from 'react-native'
+import Front from './src/components/Front'
+import Back from './src/components/Back'
 
 export default class App extends React.Component {
+  state = {
+    showingBack: false,
+    rotate: new Animated.Value(0),
+    value: 0
+  }
+
+  componentDidMount() {
+    const { rotate } = this.state
+
+    rotate.addListener(({ value }) => {
+      this.setState({
+        value
+      })
+    })
+  }
+
+  handleRotate = () => {
+    const { rotate, value, showingBack } = this.state
+
+    if (value >= 90) {
+      Animated.spring(rotate, {
+        toValue: 0,
+        tension: 10,
+        friction: 8
+      }).start()
+    } else {
+      Animated.spring(rotate, {
+        toValue: 180,
+        tension: 10,
+        friction: 8
+      }).start()
+    }
+
+    this.setState({
+      showingBack: !showingBack
+    })
+  }
+
   render() {
+    const { showingBack, rotate } = this.state
+
+    const RotateData = rotate.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['0deg', '180deg']
+    })
+
+    const RotateData1 = rotate.interpolate({
+      inputRange: [0, 180],
+      outputRange: ['180deg', '0deg']
+    })
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity onPress={this.handleRotate} style={styles.button}>
+          <Text style={styles.text}>Rotate</Text>
+        </TouchableOpacity>
+        {showingBack === false && (
+          <Animated.View
+            style={[{ transform: [{ rotateY: RotateData }] }, styles.height]}
+          >
+            <Front />
+          </Animated.View>
+        )}
+
+        {showingBack === true && (
+          <Animated.View
+            style={[{ transform: [{ rotateY: RotateData1 }] }, styles.height]}
+          >
+            <Back />
+          </Animated.View>
+        )}
+      </SafeAreaView>
+    )
   }
 }
 
@@ -16,6 +92,24 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  button: {
+    width: 200,
+    height: 30,
+    borderWidth: 1,
+    borderColor: '#9effb6',
+    backgroundColor: '#9effb6',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 3,
+    marginBottom: 10
+  },
+  text: {
+    fontSize: 18,
+    color: '#4ead64'
+  },
+  height: {
+    height: 500
+  }
+})
---
+++ src/components/Back.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+export default class Back extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={{ fontSize: 22, color: '#fff' }}> Back </Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 300,
+    height: 500,
+    backgroundColor: '#333',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#333',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
